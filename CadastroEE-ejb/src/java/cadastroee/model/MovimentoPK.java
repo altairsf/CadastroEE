db53a9dc190bbc88e70e40b6600834f60b2a13ed
altairsf <altair.regina.26@gmail.com>
2025-06-08 14:03:51 -0300

    V0

---
+++ CadastroEE-ejb/src/java/cadastroee/model/MovimentoPK.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Altair
+ */
+@Embeddable
+public class MovimentoPK implements Serializable {
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idMovimento")
+    private int idMovimento;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "Usuario_idUsuario")
+    private int usuarioidUsuario;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "Produto_idProduto")
+    private int produtoidProduto;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "Pessoa_idPessoa")
+    private int pessoaidPessoa;
+
+    public MovimentoPK() {
+    }
+
+    public MovimentoPK(int idMovimento, int usuarioidUsuario, int produtoidProduto, int pessoaidPessoa) {
+        this.idMovimento = idMovimento;
+        this.usuarioidUsuario = usuarioidUsuario;
+        this.produtoidProduto = produtoidProduto;
+        this.pessoaidPessoa = pessoaidPessoa;
+    }
+
+    public int getIdMovimento() {
+        return idMovimento;
+    }
+
+    public void setIdMovimento(int idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public int getUsuarioidUsuario() {
+        return usuarioidUsuario;
+    }
+
+    public void setUsuarioidUsuario(int usuarioidUsuario) {
+        this.usuarioidUsuario = usuarioidUsuario;
+    }
+
+    public int getProdutoidProduto() {
+        return produtoidProduto;
+    }
+
+    public void setProdutoidProduto(int produtoidProduto) {
+        this.produtoidProduto = produtoidProduto;
+    }
+
+    public int getPessoaidPessoa() {
+        return pessoaidPessoa;
+    }
+
+    public void setPessoaidPessoa(int pessoaidPessoa) {
+        this.pessoaidPessoa = pessoaidPessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) idMovimento;
+        hash += (int) usuarioidUsuario;
+        hash += (int) produtoidProduto;
+        hash += (int) pessoaidPessoa;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MovimentoPK)) {
+            return false;
+        }
+        MovimentoPK other = (MovimentoPK) object;
+        if (this.idMovimento != other.idMovimento) {
+            return false;
+        }
+        if (this.usuarioidUsuario != other.usuarioidUsuario) {
+            return false;
+        }
+        if (this.produtoidProduto != other.produtoidProduto) {
+            return false;
+        }
+        if (this.pessoaidPessoa != other.pessoaidPessoa) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.MovimentoPK[ idMovimento=" + idMovimento + ", usuarioidUsuario=" + usuarioidUsuario + ", produtoidProduto=" + produtoidProduto + ", pessoaidPessoa=" + pessoaidPessoa + " ]";
+    }
+    
+}
---
+++ CadastroEE-ejb/src/java/cadastroee/model/PessoaJuridica.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Altair
+ */
+@Entity
+@Table(name = "PessoaJuridica")
+@NamedQueries({
+    @NamedQuery(name = "PessoaJuridica.findAll", query = "SELECT p FROM PessoaJuridica p")})
+public class PessoaJuridica implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idPessoaJuridica")
+    private Integer idPessoaJuridica;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 14)
+    @Column(name = "cnpj")
+    private String cnpj;
+    @JoinColumn(name = "idPessoaJuridica", referencedColumnName = "idPessoa", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Pessoa pessoa;
+
+    public PessoaJuridica() {
+    }
+
+    public PessoaJuridica(Integer idPessoaJuridica) {
+        this.idPessoaJuridica = idPessoaJuridica;
+    }
+
+    public PessoaJuridica(Integer idPessoaJuridica, String cnpj) {
+        this.idPessoaJuridica = idPessoaJuridica;
+        this.cnpj = cnpj;
+    }
+
+    public Integer getIdPessoaJuridica() {
+        return idPessoaJuridica;
+    }
+
+    public void setIdPessoaJuridica(Integer idPessoaJuridica) {
+        this.idPessoaJuridica = idPessoaJuridica;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPessoaJuridica != null ? idPessoaJuridica.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PessoaJuridica)) {
+            return false;
+        }
+        PessoaJuridica other = (PessoaJuridica) object;
+        if ((this.idPessoaJuridica == null && other.idPessoaJuridica != null) || (this.idPessoaJuridica != null && !this.idPessoaJuridica.equals(other.idPessoaJuridica))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.PessoaJuridica[ idPessoaJuridica=" + idPessoaJuridica + " ]";
+    }
+    
+}
